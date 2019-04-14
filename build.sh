29c7cb4c45f0dcd8774c67849056a8556dd2e001
Khaled Hosny <khaledhosny@eglug.org>
2019-04-14 04:38:27 +0200

    Drop cross compilation support
    It never worked for me (because of ICU, mostly).

--- build.sh
+++ build.sh
@@ -24,11 +24,6 @@
 #      --parallel  : make -j 8 -l 8.0
 #      --nostrip   : do not strip binary
 #      --warnings= : enable compiler warnings
-#      --mingw[32] : crosscompile for mingw32
-#      --mingw64   : crosscompile for mingw64
-#      --host=     : target system for mingw32 cross-compilation
-#      --build=    : build system for mingw32 cross-compilation
-#      --arch=     : crosscompile for ARCH on OS X
 #      --clang     : use clang & clang++
 #      --debug     : CFLAGS="-g -O0" --warnings=max --nostrip
 #      --debugopt  : CFLAGS="-g -O3" --warnings=max --nostrip
@@ -65,17 +60,9 @@ BUILDTAG=
 ONLY_MAKE=FALSE
 STRIP_HARFTEX=TRUE
 WARNINGS=yes
-MINGW=FALSE
-MINGWCROSS32=FALSE
-MINGWCROSS64=FALSE
-MACCROSS=FALSE
 CLANG=FALSE
-CONFHOST=
-CONFBUILD=
 JOBS_IF_PARALLEL=${JOBS_IF_PARALLEL:-8}
 MAX_LOAD_IF_PARALLEL=${MAX_LOAD_IF_PARALLEL:-8}
-TARGET_CC=gcc
-TARGET_TCFLAGS=
 STRIPBIN=
 TEXLIVEOPT=
 
@@ -85,16 +72,11 @@ CXXFLAGS="$CXXFLAGS"
 
 until [ -z "$1" ]; do
   case "$1" in
-    --arch=*		) MACCROSS=TRUE; ARCH=`echo $1 | sed 's/--arch=\(.*\)/\1/' ` ;;
-    --build=*		) CONFBUILD="$1"     ;;
     --buildtag=*	) BUILDTAG="$1"      ;;
-    --clang		) export CC=clang; export CXX=clang++ ; TARGET_CC=$CC ; CLANG=TRUE ;;
+    --clang		) export CC=clang; export CXX=clang++ ; CLANG=TRUE ;;
     --debug		) STRIP_HARFTEX=FALSE; WARNINGS=max ; CFLAGS="-O0 -g -ggdb3 $CFLAGS" ; CXXFLAGS="-O0 -g -ggdb3 $CXXFLAGS"  ;;
     --debugopt		) STRIP_HARFTEX=FALSE; WARNINGS=max ; CFLAGS="-O3 -g -ggdb3 $CFLAGS" ; CXXFLAGS="-O3 -g -ggdb3 $CXXFLAGS"  ;;
-    --host=*		) CONFHOST="$1"      ;;
     --make		) ONLY_MAKE=TRUE     ;;
-    --mingw|--mingw32	) MINGWCROSS32=TRUE  ;;
-    --mingw64		) MINGWCROSS64=TRUE  ;;
     --nostrip		) STRIP_HARFTEX=FALSE ;;
     --parallel		) MAKE="$MAKE -j $JOBS_IF_PARALLEL -l $MAX_LOAD_IF_PARALLEL" ;;
     --stripbin=*	) STRIPBIN="$1"      ;;
@@ -111,19 +93,12 @@ HARFTEXEXE=harftex
 
 case `uname` in
   CYGWIN*	) HARFTEXEXE=harftex.exe ;;
-  MINGW*	) HARFTEXEXE=harftex.exe ;;
   Darwin	) STRIP="strip -u -r" ;;
 esac
 
 WARNINGFLAGS=--enable-compiler-warnings=$WARNINGS
 
 B=build
-
-if [ "x$CONFHOST" != "x" ]
-then
- B="build-$CONFHOST"
- B=`printf "$B"| sed 's/--host=//'`
-fi
 
 if [ "$CLANG" = "TRUE" ]
 then
@@ -135,67 +110,10 @@ then
   B="${BUILDTAG#--buildtag=}"
 fi
 
-
-OLDPATH=$PATH
-if [ "$MINGWCROSS32" = "TRUE" ]
-then
-  B=build-windows32
-  : ${CONFHOST:=--host=i686-w64-mingw32}
-  CFLAGS="-Wno-unknown-pragmas -mtune=nocona -g -O3 $CFLAGS"
-  CXXFLAGS="-Wno-unknown-pragmas -mtune=nocona -g -O3 $CXXFLAGS"
-  LDFLAGS="-Wl,--large-address-aware -Wl,--stack,2621440 $CFLAGS"
-  export CFLAGS CXXFLAGS LDFLAGS
-fi
-if [ "$MINGWCROSS64" = "TRUE" ]
-then
-  B=build-windows64
-  : ${CONFHOST:=--host=x86_64-w64-mingw32}
-  CFLAGS="-Wno-unknown-pragmas -mtune=nocona -g -O3 -fno-lto -fno-use-linker-plugin $CFLAGS"
-  CXXFLAGS="-Wno-unknown-pragmas -mtune=nocona -g -O3 -fno-lto -fno-use-linker-plugin $CXXFLAGS"
-  LDFLAGS="${LDFLAGS} -fno-lto -fno-use-linker-plugin -static-libgcc -static-libstdc++"
-  export CFLAGS CXXFLAGS LDFLAGS
-fi
-if [ "$MINGWCROSS32" = "TRUE" ] || [ "$MINGWCROSS64" = "TRUE" ]
-then
-  case `uname -s` in
-    Linux)  platform="x86_64-linux"; build_tripple="x86_64-unknown-linux-gnu" ;;
-    Darwin) platform="x86_64-darwin"; build_tripple="x86_64-apple-darwin" ;;
-  esac
-
-  : ${CONFBUILD:=--build=$build_tripple}
-  PATH=`pwd`/extrabin/mingw/$platform:/usr/mingw32/bin:$PATH
-  HARFTEXEXE=harftex.exe
-  RANLIB="${CONFHOST#--host=}-ranlib"
-  STRIP="${CONFHOST#--host=}-strip"
-fi
-
-if [ "$MACCROSS" = "TRUE" ]
-then
-  # make sure that architecture parameter is valid
-  case $ARCH in
-    i386 | x86_64 | ppc | ppc64 ) ;;
-    * ) echo "ERROR: architecture $ARCH is not supported"; exit 1;;
-  esac
-  B=build-$ARCH
-  if [ "x$CONFHOST" != "x" ]
-    then
-    B="build-$CONFHOST"
-    B=`printf "$B"| sed 's/--host=//'`
-  fi
-  CFLAGS="-arch $ARCH -g -O2 $CFLAGS"
-  CXXFLAGS="-arch $ARCH -g -O2 $CXXFLAGS"
-  LDFLAGS="-arch $ARCH $LDFLAGS" 
-  STRIP="${CONFHOST#--host=}-strip -u -r -A -n"
-  #STRIP="${CONFHOST#--host=}-strip"
-  export CFLAGS CXXFLAGS LDFLAGS
-fi
-
-
 if [ "x$STRIPBIN" != "x" ]
 then
  STRIP="${STRIPBIN#--stripbin=}"
 fi
-
 
 if [ "$STRIP_HARFTEX" = "FALSE" ]
 then
@@ -221,7 +139,7 @@ cd "$B"
 
 if [ "$ONLY_MAKE" = "FALSE" ]
 then
-TL_MAKE=$MAKE ../source/configure  $TEXLIVEOPT $CONFHOST $CONFBUILD  $WARNINGFLAGS\
+TL_MAKE=$MAKE ../source/configure  $TEXLIVEOPT $WARNINGFLAGS \
     --enable-silent-rules \
     --disable-all-pkgs \
     --disable-shared    \
@@ -246,11 +164,6 @@ then
   $STRIP "$B"/texk/web2c/$HARFTEXEXE
 else
   echo "harftex binary not stripped"
-fi
-
-if [ "$MINGWCROSS32" = "TRUE" ] || [ "$MINGWCROSS64" = "TRUE" ]
-then
-  PATH=$OLDPATH
 fi
 
 # show the result
